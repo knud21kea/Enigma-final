572c5be7d4ad6e7f942b97ed041ba17b1730e485
knud21kea <grah0070@stud.kea.dk>
2021-09-15 23:02:57 +0200

    Enigma final with all ciphers

---
+++ src/uds/knudsoft/Enigma.java
@@ -0,0 +1,268 @@
+package uds.knudsoft;
+
+import java.util.Scanner;
+
+public class Enigma {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        /*Enigma project: Number cipher, Caesar cipher, Vigenere cipher
+        Accepts danish alphabet and space, but UI in English, sorry.*/
+
+        //Intro
+        System.out.println("\nWelcome to 'Enigma'.");
+        System.out.println("This program encodes or decodes a string using one of 3 different encryption models: Number, Caesar and Vigenere ciphers.");
+        System.out.println("The user starts by choosing encode or decode, and then which cipher.");
+        System.out.println("The program quits on invalid option entry.");
+
+        /*Encode/decode? Number/Caesar/Vigenere?
+        optionMainMenu stores choices as bits:
+        bit 0 for encode/decode
+        bit 1 for Caesar ciphers instead of Number
+        bit 2 for Vigenere ciphers*/
+
+        int optionMainMenu;
+        do {
+            System.out.print("\nEnter e for encode or d for decode: ");
+            String inputMainMenu = input.nextLine().toUpperCase();
+            if (inputMainMenu.startsWith("E")) { //Encode
+                optionMainMenu = 0;
+            } else if (inputMainMenu.startsWith("D")) { //Decode
+                optionMainMenu = 1;
+            } else {
+                optionMainMenu = 7;
+            }
+            System.out.print("Enter n for Number, c for Caesar or v for Vigenere: ");
+            inputMainMenu = input.nextLine().toUpperCase();
+            if (inputMainMenu.startsWith("N")) { //Uses Number cipher by default
+            } else if (inputMainMenu.startsWith("C")) { //Use Caesar cipher
+                optionMainMenu += 2;
+            } else if (inputMainMenu.startsWith("V")) { //Use Vigenere cipher
+                optionMainMenu += 4;
+            } else {
+                optionMainMenu = 7;
+            }
+            switch (optionMainMenu) {
+                case 0 -> encodeNumberCipher();
+                case 1 -> decodeNumberCipher();
+                case 2 -> encodeCaesarCipher();
+                case 3 -> decodeCaesarCipher();
+                case 4 -> encodeVigenereCipher();
+                case 5 -> decodeVigenereCipher();
+                default -> System.out.println("Unrecognized input...");
+            }
+        }
+        while (optionMainMenu < 7);
+    }
+
+    //Encode Number
+    public static void encodeNumberCipher() {
+        System.out.println("Encoding with Number Cipher...");
+        //input a string converted to upper case
+        System.out.print("Input a string: ");
+        String inputString = input.nextLine();
+        inputString = inputString.toUpperCase();
+
+        //convert the input string to a list of numbers
+        int[] intArray = stringToNumbers(inputString);
+
+        //convert the list of numbers to a formatted string of codes
+        String outputString = convertListOfNumbersToString(intArray);
+
+        //output encoded string
+        System.out.println("The code is: " + outputString);
+    }
+
+    //Decode Number
+    public static void decodeNumberCipher() {
+        System.out.println("Decoding with Number Cipher...");
+        System.out.println("Input a list in the form {x, y, ... z}");
+        System.out.print("Or paste a previously coded list: ");
+        String inputString = input.nextLine();
+
+        //Find how many code numbers in the list
+        final int elements = getNumberOfElements(inputString);
+
+        //Extract the code numbers with custom algorithm
+        int[] codeString = new int[elements];
+        String[] subString = sliceStringIntoCodeStrings(inputString, elements);
+
+        //convert the code number strings to code numbers and output the decoded letters.
+        System.out.print("The text was: ");
+        for (int i = 0; i < elements; i++) {
+            codeString[i] = Integer.parseInt(subString[i]);
+            subString[i] = convertNumberToLetter(codeString[i]);
+            System.out.print(subString[i]);
+        }
+    }
+
+    //Encode Caesar
+    public static void encodeCaesarCipher() {
+        System.out.println("Encoding with Caesar Cipher...");
+        String outputString = commonCaesarCipher(1);
+        //output encoded string
+        System.out.println("The encoded text is: " + outputString);
+    }
+
+    //Decode Caesar
+    public static void decodeCaesarCipher() {
+        System.out.println("Decoding with Caesar Cipher...");
+        String outputString = commonCaesarCipher(-1);
+        //output encoded string
+        System.out.println("The original text was: " + outputString);
+    }
+
+    //method common to Caesar encoding/decoding controlled by a switch
+    //Space is encoded as a normal character, this improves security as the encoded text may have spaces that are not between words
+    public static String commonCaesarCipher(int sign) {
+        //input a text and a value used to shift the letters
+        System.out.print("Input a text: ");
+        String inputString = input.nextLine();
+        inputString = inputString.toUpperCase();
+        System.out.print("Input the code number: ");
+        int shiftCaesar = input.nextInt();
+        input.nextLine(); //Flush cr from the nextInt
+        shiftCaesar = (Math.abs(shiftCaesar) % 30); //only shift by 0-30
+
+        //convert the input string to a list of numbers
+        int[] intArray = stringToNumbers(inputString);
+
+        //Shift by amount sign*shiftCaesar
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = (intArray[i] + sign * shiftCaesar + 30) % 30;
+        }
+
+        //convert the list of numbers to an encoded string
+        return convertListOfNumbersToText(intArray);
+    }
+
+    //Encode Vigenere
+    public static void encodeVigenereCipher() {
+        System.out.println("Encoding with Vigenere Cipher...");
+        String outputString = commonVigenereCipher(1);
+        //output encoded string
+        System.out.println("The encoded text is: " + outputString);
+    }
+
+    //Decode Vigenere
+    public static void decodeVigenereCipher() {
+        System.out.println("Decoding with Vigenere Cipher...");
+        String outputString = commonVigenereCipher(-1);
+        //output encoded string
+        System.out.println("The original text was: " + outputString);
+    }
+
+    //method common to Vigenere encoding/decoding controlled by a switch
+    public static String commonVigenereCipher(int sign) {
+        //input a text and a codeword used to shift the letters converted to uppercase
+        System.out.print("Input a text: ");
+        String inputString = input.nextLine().toUpperCase();
+        System.out.print("Input a code word: ");
+        StringBuilder shiftVigenere = new StringBuilder(input.nextLine().toUpperCase());
+
+        //make the codeword at least as long as inputString by repeating as necessary
+        while (shiftVigenere.length() < inputString.length()) {
+            shiftVigenere.append(shiftVigenere);
+        }
+
+        //convert the input string to a list of numbers
+        int[] intArray = stringToNumbers(inputString);
+
+        //convert the code word to a list of numbers
+        int[] shiftArray = stringToNumbers(shiftVigenere.toString());
+
+        //Shift by amount sign*shiftVigenere
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = (intArray[i] + sign * shiftArray[i] + 30) % 30;
+        }
+
+        //convert the list of numbers to an encoded string
+        return convertListOfNumbersToText(intArray);
+    }
+
+    //method to convert a string to a list of numbers
+    public static int[] stringToNumbers(String inputString) {
+        int inputLength = inputString.length();
+        int[] intArray = new int[inputLength];
+        String letter;
+        for (int i = 0; i < inputLength; i++) {
+            letter = inputString.substring(i, i + 1);
+            int index = letterToInt(letter);
+            intArray[i] = index;
+        }
+        return intArray;
+    }
+
+    //method to convert 1 letter to a number
+    public static int letterToInt(String letter) {
+        final String CHARACTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        return CHARACTERS.indexOf(letter);
+    }
+
+    //method to convert a list of numbers to a text
+    public static String convertListOfNumbersToText(int[] intArray) {
+        final String CHARACTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        StringBuilder outputString = new StringBuilder();
+        for (int j : intArray) {
+            outputString.append(CHARACTERS.charAt(j));
+        }
+        return outputString.toString();
+    }
+
+    //method to convert a list of numbers to a string
+    public static String convertListOfNumbersToString(int[] intArray) {
+        int inputLength = intArray.length;
+        StringBuilder outputString = new StringBuilder("{");
+        for (int i = 0; i < inputLength - 1; i++) {
+            outputString.append(intArray[i]).append(", ");
+        }
+        outputString.append(intArray[inputLength - 1]).append("}");
+        return outputString.toString();
+    }
+
+    //method to convert 1 number to a letter
+    public static String convertNumberToLetter(int number) {
+        final String CHARACTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        return CHARACTERS.substring(number, number + 1);
+    }
+
+    //find how many elements = number of commas +1
+    public static int getNumberOfElements(String inputString) {
+        int firstComma = inputString.indexOf(",");
+        int nextComma = 0;
+        int lastComma = inputString.lastIndexOf(",", firstComma + 1);
+        int count = -1;
+        while (nextComma != lastComma) {
+            count++;
+            nextComma = inputString.indexOf(",", firstComma + 1);
+            firstComma = nextComma;
+        }
+        return count + 1;
+    }
+
+    //extract code strings from input string
+    public static String[] sliceStringIntoCodeStrings(String inputString, int codes) {
+        String[] codeString = new String[codes];
+        for (int i = 0; i < codes; i++) {
+            codeString[i] = "";
+        }
+        int code = 0;
+        int position = 0;
+        char ch = '{';
+        while (ch != '}') { //stop if end of list
+            ch = inputString.charAt(position);
+            if (Character.isDigit(ch)) {
+                while (Character.isDigit(ch)) {
+                    codeString[code] += ch;
+                    position++;
+                    ch = inputString.charAt(position);
+                }
+                code++; //move to next code string
+            }
+            position++; //point to next character
+        }
+        return codeString;
+    }
+}
